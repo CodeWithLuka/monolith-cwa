596c7028acbb9d30f4723c236e832d0b13477c19
CodeWithLuka <lukasuave.nextai@gmail.com>
2026-07-11 08:29:14 +0200

    feat 08: Setup Vercel AI SDK

--- src/app/(dashboard)/(main)/workflows/page.tsx
+++ src/app/(dashboard)/(main)/workflows/page.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 const WorkflowsPage = () => {
   const trpc = useTRPC();
@@ -17,6 +18,14 @@ const WorkflowsPage = () => {
     }),
   );
 
+  const testAi = useMutation(
+    trpc.testAI.mutationOptions({
+      onSuccess: () => {
+        toast.success("Lock Off");
+      },
+    }),
+  );
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black gap-6">
       <h1 className="text-3xl"> Think, Workflows</h1>
@@ -25,6 +34,9 @@ const WorkflowsPage = () => {
         onClick={() => createWorkflow.mutate()}
       >
         Create Workflow
+      </Button>
+      <Button disabled={testAi.isPending} onClick={() => testAi.mutate()}>
+        Test AI
       </Button>
       <p>{JSON.stringify(workflows, null, 2)}</p>
     </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,23 +1,25 @@
+import { generateText } from "ai";
+
+import { createGoogleGenerativeAI, google } from "@ai-sdk/google";
+
 import prisma from "@/db";
+
 import { inngest } from "./client";
 
-export const createWorkflow = inngest.createFunction(
-  { id: "create-workflow", triggers: { event: "create/workflow" } },
+// const goole = createGoogleGenerativeAI()
+
+export const testAI = inngest.createFunction(
+  { id: "blue-lock", triggers: { event: "blue/lock" } },
   async ({ event, step }) => {
-    await step.sleep("creating-name", "5s");
-
-    await step.sleep("fetching-name", "5s");
-
-    await step.sleep("show-name", "5s");
-
-    const createdWorkflow = await step.run("create-blue-lock-workflow", () => {
-      return prisma.workflow.create({
-        data: {
-          name: "blue-lock-inngest",
-        },
-      });
+    const { text } = await step.ai.wrap("blue-lock-ai", generateText, {
+      model: google("gemini-3.5-flash"),
+      instructions:
+        "You are a blue lock mega fan and up to date with all things blue lock like manga, anime and works of the mangaka",
+      prompt:
+        "Who is Yoichi Isagi and will he be the ultimate egoist in Blue Lock",
+      reasoning: "high",
     });
 
-    return { message: `Workflow ${event.data.id} created`, createdWorkflow };
+    return { message: `Blue Lock ${event.data.id}`, text };
   },
 );
